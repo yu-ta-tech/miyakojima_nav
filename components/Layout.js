c37b20340cfeee06dd7c479c0ef2f7e5844ac463
Yuta Ueda <yuta8722@icloud.com>
2023-04-19 23:43:47 +0900

    header

--- components/Layout.js
+++ components/Layout.js
@@ -9,23 +9,34 @@ export default function Layout({ children, title = "みやこナビ" }) {
         <title>{title}</title>
       </Head>
       <header className="sticky top-0">
-        <nav className="bg-gray-800 w-screen">
-          <div className="flex flex-row-reverse items-center pr-32 h-14">
+        <nav className="h-14 bg-gray-800 w-screen">
+          <div className="float-left text-xl text-gray-300 pl-32 mt-3">
+            <a href="#">みやこナビ</a>
+          </div>
+          <div className="float-right pr-32  mt-3">
             <div className="flex space-x-4">
               <Link href="/">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Home</a>
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Home
+                </a>
               </Link>
               <Link href="/map-page">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Map</a>
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Map
+                </a>
               </Link>
               <Link href="#">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Contact</a>
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                  Contact
+                </a>
               </Link>
             </div>
           </div>
         </nav>
       </header>
-      <main className="flex flex-1 justify-center items-center flex-col  w-screen">{children}</main>
+      <main className="flex flex-1 justify-center items-center flex-col  w-screen">
+        {children}
+      </main>
       <footer>
         <div>&copy; みやこナビ</div>
       </footer>
