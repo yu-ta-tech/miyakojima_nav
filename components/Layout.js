4770feec4f5b654bfc6fc8dd4a508b671f7eb278
Yuta Ueda <yuta8722@icloud.com>
2023-04-19 10:59:53 +0900

    sticky

--- components/Layout.js
+++ components/Layout.js
@@ -4,25 +4,28 @@ import Image from "next/image";
 
 export default function Layout({ children, title = "みやこナビ" }) {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center text-gray-600 text-sm font-mono">
       <Head>
         <title>{title}</title>
       </Head>
-      <header>
-        <nav>
-          <div>
-            <div>
+      <header className="sticky top-0">
+        <nav className="bg-gray-800 w-screen">
+          <div className="flex flex-row-reverse items-center pr-32 h-14">
+            <div className="flex space-x-4">
               <Link href="/">
-                <a>ホーム</a>
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Home</a>
               </Link>
               <Link href="/map-page">
-                <a>マップ</a>
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Map</a>
+              </Link>
+              <Link href="#">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Contact</a>
               </Link>
             </div>
           </div>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className="flex flex-1 justify-center items-center flex-col  w-screen">{children}</main>
       <footer>
         <div>&copy; みやこナビ</div>
       </footer>
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div title="Home" className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
-      <h1>ようこそ みやこナビへ</h1>
+    <Layout title="Home" className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
+      <h1 className="text-4xl">ようこそ みやこナビへ</h1>
       <p>あああああああ</p>
       <p>あああああああ</p>
       <p>あああああああ</p>
-    </div>
+    </Layout>
   );
 }
