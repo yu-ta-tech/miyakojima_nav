4b7b2e753f727095aa2e1f00554cbef8a5f93501
Yuta Ueda <yuta8722@icloud.com>
2023-04-26 00:05:09 +0900

    update SpotMap component

--- components/SpotMap.js
+++ components/SpotMap.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import Map, {
   Layer,
   LayerProps,
@@ -11,21 +12,29 @@ import Map, {
   FullscreenControl,
 } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { spots } from "../lib/spots";
 
 export default function SpotMap() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  //router.queryが初回レンダリング時にundefinedになるため
+  if (!router.isReady) {
+    return null;
+  }
   return (
     <Map
       id="spotMap"
       initialViewState={{
-        longitude: "125.3246",
-        latitude: "24.7673",
-        zoom: 11,
+        longitude: spots[id].longitude,
+        latitude: spots[id].latitude,
+        zoom: 12,
       }}
       style={{ top: "20px", bottom: "20px", width: 448, height: 408 }}
       mapStyle={"mapbox://styles/mapbox/light-v10"}
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_BOX_TOKEN}
     >
-      <Marker longitude={125.3246} latitude={24.7673} anchor="bottom">
+      <Marker longitude={spots[id].longitude} latitude={spots[id].latitude} anchor="bottom">
         <Image src="/images/marker.png" width={30} height={30} />
       </Marker>
       <NavigationControl />
