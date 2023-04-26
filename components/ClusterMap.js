5c0995bdc0af6800a3294336e47badeb71bdfc1e
Yuta Ueda <yuta8722@icloud.com>
2023-04-26 23:46:07 +0900

    popup 表示非表示OK

--- components/ClusterMap.js
+++ components/ClusterMap.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { useState } from "react";
 import { useRef } from "react";
 import { spots } from "../lib/spots_detailData";
 import Map, {
@@ -11,6 +11,7 @@ import Map, {
   Source,
   useMap,
   FullscreenControl,
+  Popup,
 } from "react-map-gl";
 import {
   clusterLayer,
@@ -20,26 +21,35 @@ import {
 import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function ClusterMap() {
+  const [showPopup, setShowPopup] = useState(false);
+
   const mapRef = useRef(null);
 
+  // 
   const onClick = (event) => {
-    const feature = event.features[0];
-    const clusterId = feature.properties.cluster_id;
-
-    const mapboxSource = mapRef.current.getSource("earthquakes");
-
-    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
-      if (err) {
-        return;
-      }
-
-      mapRef.current.easeTo({
-        center: feature.geometry.coordinates,
-        zoom,
-        duration: 500,
-      });
-    });
+    // const lon = event.longitude;
+    // const lat = event.latitude;
+    showPopup ? setShowPopup(false) : setShowPopup(true);
   };
+
+  // const onClick = (event) => {
+  //   const feature = event.features[0];
+  //   const clusterId = feature.properties.cluster_id;
+
+  //   const mapboxSource = mapRef.current.getSource("earthquakes");
+
+  //   mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+  //     if (err) {
+  //       return;
+  //     }
+
+  //     mapRef.current.easeTo({
+  //       center: feature.geometry.coordinates,
+  //       zoom,
+  //       duration: 500,
+  //     });
+  //   });
+  // };
   return (
     <Map
       id="ClusterMap"
@@ -55,7 +65,7 @@ export default function ClusterMap() {
       onClick={onClick}
     >
       {/* clustermapにしたいけど、できてない。https://visgl.github.io/react-map-gl/examples/clusters */}
-      <Source
+      {/* <Source
         id="earthquakes"
         type="geojson"
         data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
@@ -66,16 +76,38 @@ export default function ClusterMap() {
         <Layer {...clusterLayer} />
         <Layer {...clusterCountLayer} />
         <Layer {...unclusteredPointLayer} />
-      </Source>
+      </Source> */}
 
       {spots &&
-        spots.map((spot) => (
-          <Marker
-            longitude={spot.longitude}
-            latitude={spot.latitude}
-            anchor="bottom"
-          />
+        spots.map((spot, i) => (
+          <div key={i}>
+            <Marker
+              longitude={spot.longitude}
+              latitude={spot.latitude}
+              anchor="bottom"
+            />
+            {/* {showPopup && (
+              <Popup
+                longitude={spot.longitude}
+                latitude={spot.latitude}
+                anchor="bottom"
+                onClose={() => setShowPopup(false)}
+              >
+                aaa
+              </Popup>
+            )} */}
+          </div>
         ))}
+      {showPopup && (
+        <Popup
+          longitude={spots[0].longitude}
+          latitude={spots[0].latitude}
+          anchor="bottom"
+          onClose={() => setShowPopup(false)}
+        >
+          aaa
+        </Popup>
+      )}
       <NavigationControl />
     </Map>
   );
