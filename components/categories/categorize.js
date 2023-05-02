7758b58fc88e26f86454cb364e2a5b485565c2c1
Yuta Ueda <yuta8722@icloud.com>
2023-05-03 02:01:09 +0900

    カテゴリー分け

--- components/categories/categorize.js
+++ components/categories/categorize.js
@@ -3,43 +3,45 @@ import Image from "next/image";
 import { places } from "../../lib/spotLists";
 import { spots } from "../../lib/spots_detailData";
 
-export default function Event() {
-  const beaches = places.filter((place) => place.category === "event");
+export default function Categorize({ category }) {
+  const categorizedPlaces = places.filter(
+    (place) => place.category === category
+  );
 
   return (
     <div className="text-xl mx-4">
-      {beaches &&
-        beaches.map((event) => (
+      {categorizedPlaces &&
+        categorizedPlaces.map((categorizedPlace) => (
           <div
-            key={event.id}
+            key={categorizedPlace.id}
             className="max-w-sm w-full lg:max-w-7xl lg:flex my-3"
           >
             <div className="flex-shrink-0 grid">
               <Image
-                src={`/images/${places.indexOf(event)}.jpg`}
+                src={`/images/${places.indexOf(categorizedPlace)}.jpg`}
                 width={312}
                 height={208}
-                alt={event.name}
+                alt={categorizedPlace.name}
               />
             </div>
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
               <div className="text-gray-700 font-bold text-xl pb-1">
-                {event.name}
+                {categorizedPlace.name}
               </div>
               <div className="px-1 pb-3">
                 <p className="text-gray-700 text-base">
-                  {spots[event.id].description}
+                  {spots[categorizedPlace.id].description}
                 </p>
               </div>
               <div className="flex items-center px-1 pb-4">
                 <div className="text-sm">
-                  <p className="text-gray-500">{event.address}</p>
+                  <p className="text-gray-500">{categorizedPlace.address}</p>
                 </div>
               </div>
               <div>
-                <Link href={`/locations/${places.indexOf(event)}`}>
+                <Link href={`/locations/${places.indexOf(categorizedPlace)}`}>
                   <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm rounded px-2 h-8">
-                    {`${event.name}の詳細`}
+                    {`${categorizedPlace.name}の詳細`}
                   </button>
                 </Link>
               </div>
--- pages/test_spotpage.js
+++ pages/test_spotpage.js
@@ -5,35 +5,27 @@ import { places } from "../lib/spotLists";
 import { spots } from "../lib/spots_detailData";
 import AllMap from "../components/maps/AllMap";
 import AllSpots from "../components/categories/allSpots";
-import Beach from "../components/categories/beach";
-import Food from "../components/categories/food";
-import Shopping from "../components/categories/shopping";
-import Event from "../components/categories/event";
-import Fitness from "../components/categories/fitness";
+import Categorize from "../components/categories/categorize";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 const Spot = () => {
-  const onSelect = (e) => {
-    console.log(e);
-  };
-
   return (
     <Layout title="見どころ一覧">
       <h1 className="text-4xl pt-4 underline">全体マップ</h1>
       <div className="flex justify-center w-full h-72 md:h-96 lg:h-96">
         <AllMap />
       </div>
-      <h1 className="text-3xl pt-6 mt-2 font-bold">
+      <h1 className="text-3xl pt-6 pb-2 mt-2 font-bold">
         見どころ一覧（ジャンル別）
       </h1>
-      <Tabs onSelect={onSelect}>
-        <TabList>
+      <Tabs defaultCSS={false}>
+        <TabList className="w-full pl-4 lg:pl-8 border-b border-gray-400">
           <Tab>全体</Tab>
           <Tab>ビーチ</Tab>
           <Tab>グルメ</Tab>
           <Tab>買い物</Tab>
-          <Tab>イベント</Tab>
+          {/* <Tab>イベント</Tab> */}
           <Tab>フィットネス</Tab>
         </TabList>
 
@@ -41,19 +33,19 @@ const Spot = () => {
           <AllSpots />
         </TabPanel>
         <TabPanel>
-          <Beach />
+          <Categorize category={"beach"} />
         </TabPanel>
         <TabPanel>
-          <Food />
+          <Categorize category={"food"} />
         </TabPanel>
         <TabPanel>
-          <Shopping />
+          <Categorize category={"shopping"} />
         </TabPanel>
+        {/* <TabPanel>
+          <Categorize category={"event"} />
+        </TabPanel> */}
         <TabPanel>
-          <Event />
-        </TabPanel>
-        <TabPanel>
-          <Fitness />
+          <Categorize category={"fitness"} />
         </TabPanel>
       </Tabs>
     </Layout>
